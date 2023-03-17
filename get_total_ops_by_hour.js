ada67f236896d337af9b327a9d1fadd58b24ecd7
Paul Comeau <122116021+pjcmongo@users.noreply.github.com>
2023-03-17 08:20:53 -0700

    Update get_total_ops_by_hour.js
    added projection to get_total_ops_by_hour

--- get_total_ops_by_hour.js
+++ get_total_ops_by_hour.js
@@ -1,5 +1,10 @@
+//
+// MongoDB aggregation to get total number of OpLog entries by hour
+//
+
 setDatePartsTZ = { $set: {datePartsTZ: {$dateToParts: {date:'$wall'}}}}
 groupfield = { year: "$datePartsTZ.year", month: "$datePartsTZ.month", day: "$datePartsTZ.day", hour: "$datePartsTZ.hour" }
 groupstage = { $group: { _id: groupfield, totalOps: { $sum: 1} } }
 sort2 = { $sort: {_id:1 }}
-printjson(db.getSiblingDB("local").oplog.rs.aggregate([setDatePartsTZ, groupstage,sort2]).toArray())
+proj = { $project: { _id:0, hour: { $concat: [ {$toString:"$_id.month"}, "-", {$toString:"$_id.day"}, "-", {$toString:"$_id.year"}, "T", {$toString:"$_id.hour"}, ":00"] }, totalOps:1 } }
+printjson(db.getSiblingDB("local").oplog.rs.aggregate([setDatePartsTZ, groupstage,sort2,proj]).toArray())
